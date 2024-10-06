4c32a2d5d1834ba39e8ff904fb7c45907033e3f6
Till Gerken <till.gerken@gmail.com>
2024-10-06 17:51:46 +0200

    Add support for env variable override

--- lib/src/proxied_azure_cli_credential.dart
+++ lib/src/proxied_azure_cli_credential.dart
@@ -5,10 +5,11 @@ import 'package:http/http.dart' as http;
 import 'package:azure_identity/src/token_credential.dart';
 
 /// Attempts to acquire a token through a proxied instance of Azure CLI.
-/// The output of Azure CLI is expected at http://localhost:8181. This
-/// token is intended to be used in development settings where the application
-/// is running locally inside a Docker container but does not have access to
-/// Azure CLI inside the container.
+/// The output of Azure CLI is expected at http://localhost:8181 by default
+/// and can be overridden by setting the environment variable
+/// AZURE_CLI_PROXY_HOST. This token is intended to be used in development
+/// settings where the application is running locally inside a Docker container
+/// but does not have access to Azure CLI inside the container.
 class ProxiedAzureCliCredential extends TokenCredential {
   ProxiedAzureCliCredential({super.logger});
 
@@ -21,7 +22,12 @@ class ProxiedAzureCliCredential extends TokenCredential {
     final resource = options.scopes.first;
 
     try {
-      final response = await http.get(Uri.parse('http://localhost:8181/'));
+      var proxyHost = Platform.environment['AZURE_CLI_PROXY_HOST'];
+      proxyHost ??= 'http://host.docker.internal:8181/';
+
+      proxyHost = '$proxyHost?resource=$resource';
+
+      final response = await http.get(Uri.parse(proxyHost));
       if (response.statusCode != 200) {
         logger?.call('Unable to access token proxy at http://localhost:8181.');
         return null;
